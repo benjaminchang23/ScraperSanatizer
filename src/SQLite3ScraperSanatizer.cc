53bf5ebbb9d5a276f333a50f109e2796f2c16930
Benjamin Chang <benjaminchang23@gmail.com>
2021-06-01 20:14:06 -0400

    feat: only allow printable characters, unallow semicolon

--- src/SQLite3ScraperSanatizer.cc
+++ src/SQLite3ScraperSanatizer.cc
@@ -11,6 +11,19 @@ namespace scraper_sanatizer {
 int SQLite3ScraperSanatize(std::string &target_string)
 {
     target_string.erase(std::remove(target_string.begin(), target_string.end(), '\r'), target_string.end());
+    target_string.erase(std::remove(target_string.begin(), target_string.end(), '\n'), target_string.end());
+    target_string.erase(std::remove(target_string.begin(), target_string.end(), ' '), target_string.end());
+    target_string.erase(std::remove(target_string.begin(), target_string.end(), '\n'), target_string.end());
+    target_string.erase(std::remove(target_string.begin(), target_string.end(), '\0'), target_string.end());
+    target_string.erase(std::remove(target_string.begin(), target_string.end(), ';'), target_string.end());
+
+    // remove unprintable characters
+    target_string.erase(std::remove_if(target_string.begin(), target_string.end(), 
+        [](unsigned char c)
+        {
+            return !std::isprint(c);
+        })
+        , target_string.end());
 
     return 0;
 }
