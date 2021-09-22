a383074115ba9f607be926faacfb31c1544d41df
Benjamin Chang <benjaminchang23@gmail.com>
2021-09-22 19:15:58 -0400

    feat: add unicode decode for uri

--- src/SQLite3ScraperSanatizer.cc
+++ src/SQLite3ScraperSanatizer.cc
@@ -8,6 +8,41 @@
 
 namespace scraper_sanatizer {
 
+// based on https://stackoverflow.com/questions/2673207/c-c-url-decode-library/2766963
+void urldecode(char *dst, const char *src)
+{
+    char a, b;
+    while (*src) {
+        if ((*src == '%') && ((a = src[1]) && (b = src[2])) && (isxdigit(a) && isxdigit(b)))
+        {
+            if (a >= 'a')
+                a -= 'a'-'A';
+            if (a >= 'A')
+                a -= ('A' - 10);
+            else
+                a -= '0';
+            if (b >= 'a')
+                b -= 'a'-'A';
+            if (b >= 'A')
+                b -= ('A' - 10);
+            else
+                b -= '0';
+            *dst++ = 16*a+b;
+            src+=3;
+        }
+        else if (*src == '+')
+        {
+            *dst++ = ' ';
+            src++;
+        }
+        else
+        {
+            *dst++ = *src++;
+        }
+    }
+    *dst++ = '\0';
+}
+
 int SQLite3ScraperSanatize(std::string &target_string)
 {
     target_string.erase(std::remove(target_string.begin(), target_string.end(), '\r'), target_string.end());
@@ -24,6 +59,13 @@ int SQLite3ScraperSanatize(std::string &target_string)
             return !std::isprint(c);
         })
         , target_string.end());
+
+    // decode uri unicode
+    char output[target_string.size()];
+
+    urldecode(output, target_string.c_str());
+
+    target_string = std::string(output);
 
     return 0;
 }
